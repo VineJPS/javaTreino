707fcfaee3c81bd0729c089f87755358cd89d0fd
VineJPS <vinicius108sousa@gmail.com>
2025-02-17 21:41:00 -0300

    teste de envio

--- main.java
+++ main.java
@@ -1,3 +1,10 @@
 public class main {
+    public static void main(String[] args){
+        int minhaidade = 18; //tipo nome e valor da variavel
+
+        var nomeVariavel = "Vinicius"; //Ele auto atribui o tipo com base no valor recebido
+
+        minhaidade = 20;
+    }
 
 }
